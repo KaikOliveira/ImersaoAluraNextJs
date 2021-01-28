791e5113ac6d3a7023bb32fcd5ac3ef13cc8b87a
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-01-28 05:43:03 -0300

    up

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -5,22 +5,22 @@ export default function SiteHead() {
   return (
     <Head>
       <title>Quiz Ciencia | Kaik Oliveira</title>
-      <link rel="icon" href="/img/mjolnir.svg" />
+      <link rel="icon" href="https://svgsilh.com/svg/30442.svg" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-      <meta property="og:description" content="Quiz sobre o Universo Marvel" />
+      <meta property="og:description" content="Quiz sobre de Ciencias" />
       <meta property="og:locale" content="pt_BR" />
-      <meta property="og:site_name" content="Feliquiz Marvel" />
-      <meta property="og:url" content="https://quiz-with-nextjs.vercel.app/" />
-      <meta property="og:image" content="/img/ogimage.png" />
+      <meta property="og:site_name" content="Quiz de Ciencias" />
+      <meta property="og:url" content="https://quiz-ciencia-alura.vercel.app/" />
+      <meta property="og:image" content="" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="660" />
-      <meta property="og:image:alt" content="Quiz Marvel" />
-      <meta property="twitter:description" content="Quiz sobre o Universo Marvel" />
-      <meta property="twitter:creator" content="Davi Félix" />
-      <meta property="twitter:image" content="/img/ogimage.png" />
-      <meta property="twitter:site" content="Feliquiz Marvel" />
+      <meta property="og:image:alt" content="Quiz Ciencias" />
+      <meta property="twitter:description" content="Quiz sobre Ciencias" />
+      <meta property="twitter:creator" content="Kaik Oliveira" />
+      <meta property="twitter:image" content="" />
+      <meta property="twitter:site" content="Quiz sobre Ciencias" />
     </Head>
   );
 }
